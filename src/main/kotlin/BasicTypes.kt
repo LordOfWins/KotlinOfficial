d9d0d07c674be63093c97d8f80c2c920bed3f821
윤승재 <sjyun011278@gmail.com>
2023-08-25 23:30:41 +0900

    [FEAT] : ⚙️: 코틀린 기본타입

---
+++ src/main/kotlin/BasicTypes.kt
@@ -0,0 +1,16 @@
+fun main() {
+//코틀린은 자동 추론 가능
+
+//변수를 선언하고 나중에 초기화 할 수 있는데 초기화를 안 하려면 :(콜론)을 붙여 준다.
+    val name = "Kotlin"
+    println(name) // Kotlin
+
+    // Some customers leave the queue
+    var customers = 8
+    customers += 3 // Example of addition: 11
+    customers += 7            // Example of addition: 18
+    customers -= 3            // Example of subtraction: 15
+    customers *= 2            // Example of multiplication: 30
+    customers /= 3            // Example of division: 10
+    println(customers) // 10
+}
